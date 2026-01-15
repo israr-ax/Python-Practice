709016dc001db7520bc8878013dd7f5cd73d608b
israr-ax <israrmemon211@gmail.com>
2026-01-15 16:20:31 +0500

    Add tuples.py

--- foundations/tuples.py
+++ foundations/tuples.py
@@ -1,5 +1,5 @@
 # 1- Create a tuple and convert it into a list.
-print("Create a tuple and convert it into a list.")
+print("Q:1- Create a tuple and convert it into a list.")
 my_tuple=("Ali","Ahmed","Esrar","John")
 print(type(my_tuple))
 print(my_tuple)
@@ -8,9 +8,18 @@ print(type(my_List))
 print(my_List)
 
 # 2-Count the frequency of an element in a tuple
-print("Count the frequency of an element in a tuple")
+print("Q:2- Count the frequency of an element in a tuple")
 
 my_tuple2=(1,3,4,3,5,2,1,6,3,1)
 num=3
 freq=my_tuple2.count(num)
 print(f"{my_tuple2} \n{num} appears {freq} times ")
+
+# Find the index of an element in a tuple
+print("Q:3- Find the index of an element in a tuple")
+
+print(my_tuple2)
+rem_depli=[x for i, x in enumerate(my_tuple2) if x not in my_tuple2[:i]]
+element=3
+fin_index=rem_depli.index(element)
+print(f"Remove Duplicate elements from tuples: {rem_depli} \nIndex of {element} : {fin_index}")
