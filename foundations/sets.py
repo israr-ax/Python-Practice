e6a182b7bdb8b1912cfe306ba0d5923df0ae4975
israr-ax <israrmemon211@gmail.com>
2026-01-18 12:29:59 +0500

    sets.py

---
+++ foundations/sets.py
@@ -0,0 +1,6 @@
+# 1. Find Common Elements Between Two Sets
+print("1. Find Common Elements Between Two Set")
+set1={2,3,4,5,6,7}
+set2={1,3,5,7,2.9}
+common= set1 & set2
+print(f"set1: {set1}\nset2: {set2} \nCommon Element Between Tow sets:{common}")
