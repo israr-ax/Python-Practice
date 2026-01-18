dea40973571b6eede45789e6118eca7064aecfd3
israr-ax <israrmemon211@gmail.com>
2026-01-18 16:20:50 +0500

    New conditions.py

---
+++ foundations/conditions.py
@@ -0,0 +1,11 @@
+# 1. Check Whether a Number Is Positive, Negative, or Zero
+print("1. Check Whether a Number Is Positive, Negative, or Zero")
+
+num=int(input("Enter any number:"))
+if num>0:
+    print("Positive")
+elif num<0:
+    print("Negative")
+else:
+    print("Zero")
+    
