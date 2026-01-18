d43b3a31b7a3e3d0951eb45e2702eddb888cb513
israr-ax <israrmemon211@gmail.com>
2026-01-18 14:18:41 +0500

    sets.py

--- foundations/sets.py
+++ foundations/sets.py
@@ -16,3 +16,7 @@ print("3. Remove Duplicates From a List Using Sets")
 num1=[1,2,3,2,3,5,4,4,5,6,7,6,7]
 uniq_num=set(num1)
 print(f"List: {num1} \nRemove dubplicates elements from List: {uniq_num}")
+
+# 4. Perform Union, Intersection, and Difference on Two Sets
+print("4. Perform Union, Intersection, and Difference on Two Sets")
+print(f"Set_1: {set1}. \nSet_2: {set2} \nUnion Of Set_1 and Set_2: {set1 | set2}. \nIntersection Of Set_1 and Set_2: {set1 & set2}. \nDifference Of Set_1 and Set_2: {set1 - set2}.")
